fca86e425f0d0571f0b5a08471d30a7a945ced58
unknown <iteacup3@gmail.com>
2025-04-30 01:19:56 -0500

    fixed logging 2

--- main.js
+++ main.js
@@ -28,8 +28,8 @@ main(); //run once at startup, then run the schedule below:
 //For example, cron.schedule('0 0-23 * * *', () => { }, { timezone: 'America/Chicago' });
 //minute(0-59) hour(0-23) dayOfMonth(1-31) month(1-12) dayOfWeek(1-7)
 
-//cron.schedule('*/5 * * * *', // Schedule task to run every 5 minutes
-cron.schedule('0 2 * * *', // Schedule task to run every day at specific time (e.g., 2:00 AM)
+cron.schedule('*/2 * * * *', // Schedule task to run every 5 minutes
+//cron.schedule('0 2 * * *', // Schedule task to run every day at specific time (e.g., 2:00 AM)
     main, { 
     scheduled: true,
     timezone: process.env.timezone
